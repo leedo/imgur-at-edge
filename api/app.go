4fd2a07af34fafc5c7eae1ffe907cd36eb7f164f
Lee Aylward <laylward@fastly.com>
2026-06-20 14:58:17 -0400

    move trace code to separate file

--- api/app.go
+++ api/app.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"imgur-at-edge/media"
@@ -21,7 +20,6 @@ const (
 	jsonType  = "application/json"
 	cacheHit  = "HIT"
 	cacheMiss = "MISS"
-	traceKey  = "trace"
 )
 
 type App struct {
@@ -29,38 +27,6 @@ type App struct {
 	ValidateBufLength int
 	KVStore           *kvstore.Store
 	TTL               time.Duration
-}
-
-type span struct {
-	name     string
-	start    time.Time
-	duration int64
-	childs   []*span
-}
-
-func NewTrace(name string) *span {
-	return &span{name: name, start: time.Now()}
-}
-
-func (s *span) End() {
-	s.duration = time.Since(s.start).Microseconds()
-}
-
-func (s span) String() string {
-	if s.duration == 0 {
-		s.End()
-	}
-	var out []string
-	for _, c := range s.childs {
-		out = append(out, " ["+c.String()+"]")
-	}
-	return s.name + "=" + strconv.FormatInt(s.duration, 10) + "μs" + strings.Join(out, "")
-}
-
-func (s *span) AddSpan(name string) *span {
-	c := &span{name: name, start: time.Now()}
-	s.childs = append(s.childs, c)
-	return c
 }
 
 func (a *App) putHandler() func(w http.ResponseWriter, r *http.Request) {
@@ -127,13 +93,6 @@ func sendPutOK(w http.ResponseWriter, r *http.Request, key string, ext string) {
 
 	const js = `{"status": "ok", "data": {"id": "%s", "link": "https://%s/v2/%s.%s"}}`
 	fmt.Fprintf(w, js, key, r.URL.Host, key, ext)
-}
-
-func requestTrace(r *http.Request) *span {
-	if t := r.Context().Value(traceKey); t != nil {
-		return t.(*span)
-	}
-	return NewTrace("unknown")
 }
 
 func (a *App) getHandlerV2() func(w http.ResponseWriter, r *http.Request) {
@@ -285,18 +244,6 @@ func (a *App) getOrSet(key string, t *span) (io.Reader, string, error) {
 	})
 
 	return res, hit, err
-}
-
-func traceMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		routeName := "unknown"
-		if route := mux.CurrentRoute(r); route != nil {
-			routeName = route.GetName()
-		}
-
-		ctx := context.WithValue(r.Context(), traceKey, NewTrace(routeName))
-		next.ServeHTTP(w, r.WithContext(ctx))
-	})
 }
 
 func (a *App) Router() *mux.Router {
---
+++ api/trace.go
@@ -0,0 +1,64 @@
+package api
+
+import (
+	"context"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+const traceKey = "trace"
+
+type span struct {
+	name     string
+	start    time.Time
+	duration int64
+	childs   []*span
+}
+
+func NewTrace(name string) *span {
+	return &span{name: name, start: time.Now()}
+}
+
+func (s *span) End() {
+	s.duration = time.Since(s.start).Microseconds()
+}
+
+func (s span) String() string {
+	if s.duration == 0 {
+		s.End()
+	}
+	var out []string
+	for _, c := range s.childs {
+		out = append(out, " ["+c.String()+"]")
+	}
+	return s.name + "=" + strconv.FormatInt(s.duration, 10) + "μs" + strings.Join(out, "")
+}
+
+func (s *span) AddSpan(name string) *span {
+	c := &span{name: name, start: time.Now()}
+	s.childs = append(s.childs, c)
+	return c
+}
+
+func requestTrace(r *http.Request) *span {
+	if t := r.Context().Value(traceKey); t != nil {
+		return t.(*span)
+	}
+	return NewTrace("unknown")
+}
+
+func traceMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		routeName := "unknown"
+		if route := mux.CurrentRoute(r); route != nil {
+			routeName = route.GetName()
+		}
+
+		ctx := context.WithValue(r.Context(), traceKey, NewTrace(routeName))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
